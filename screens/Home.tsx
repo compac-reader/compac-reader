1412a4155617718a6b7547601bbc22277197ccf8
Kouki Saito <dan.addr.skd@gmail.com>
2022-02-09 09:26:39 +0900

    Remove unnecessary data insertion to test
    #25 offers the function to add any stories from websites.

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -7,7 +7,7 @@ import { SectionHeader } from "../components/SectionHeader";
 import { StoryList } from "../components/StoryList";
 import { Story } from "../models/story";
 import { useColors } from "../hooks/useColors";
-import { insertStory, queryStories } from "../database";
+import { queryStories } from "../database";
 
 type HomeScreenNavigationProp = NativeStackNavigationProp<
   RootStackParamList,
@@ -28,22 +28,6 @@ export function Home() {
   const navigation = useNavigation<HomeScreenNavigationProp>();
   const stories = useStories();
   const colors = useColors();
-  useEffect(() => {
-    (async () => {
-      // TODO: remove after implementing a feature to add a new story
-      await insertStory({
-        id: "narou__n6829bd",
-        publisherType: "narou",
-        publisherCode: "n6829bd",
-        title: "賢者の弟子を名乗る賢者",
-        authorName: "りゅうせんひろつぐ",
-        description: `仮想空間に構築された世界の一つ。鑑(かがみ)は、その世界で九賢者という術士の最高位に座していた。
-ある日、徹夜の疲れから仮想空間の中で眠ってしまう。そして目を覚ますと、そこは今までの世界とは違い……。何よりも、慣れ親しんだ渋く老練とした威厳のある姿が、幼気な少女のものになってしまっていた。
-何者かと問われ、正直に名乗れば今まで培ってきた荘厳なイメージを崩しかねない状況。そこで思いついた言い訳は……。`,
-        lastUpdatedAt: Math.floor(Date.now() / 1000),
-      });
-    })();
-  }, []);
 
   return (
     <View style={{ ...styles.container, backgroundColor: colors.background }}>
